ce3c97910e6aaa4654c40c3c368cede8f8958122
Kyrylo Orlov <dev@kyryloorlov.com>
2023-09-19 18:44:13 -0400

    Remove useless fonts

--- styles/fonts.js
+++ styles/fonts.js
@@ -5,11 +5,6 @@ const haffer = localFont({
   variable: '--font',
   src: [
     {
-      path: '../assets/fonts/HafferSQ-Light.woff2',
-      weight: '300',
-      style: 'normal',
-    },
-    {
       path: '../assets/fonts/HafferSQ-Regular.woff2',
       weight: '400',
       style: 'normal',
@@ -18,11 +13,6 @@ const haffer = localFont({
       path: '../assets/fonts/HafferSQ-RegularItalic.woff2',
       weight: '400',
       style: 'italic',
-    },
-    {
-      path: '../assets/fonts/HafferSQ-Medium.woff2',
-      weight: '500',
-      style: 'normal',
     },
     {
       path: '../assets/fonts/HafferSQ-Bold.woff2',
