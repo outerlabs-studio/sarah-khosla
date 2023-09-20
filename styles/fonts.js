306b23ce83d3e5baea306d1e0fac5333202a3f41
Kyrylo Orlov <dev@kyryloorlov.com>
2023-09-20 19:53:53 -0400

    Adjust font docs

--- styles/fonts.js
+++ styles/fonts.js
@@ -3,19 +3,20 @@ import localFont from 'next/font/local'
 const haffer = localFont({
   adjustFontFallback: 'Arial',
   variable: '--font',
+  preload: true,
   src: [
     {
-      path: '../assets/fonts/HafferSQ-Regular.woff2',
+      path: '../public/fonts/HafferSQ-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../assets/fonts/HafferSQ-RegularItalic.woff2',
+      path: '../public/fonts/HafferSQ-RegularItalic.woff2',
       weight: '400',
       style: 'italic',
     },
     {
-      path: '../assets/fonts/HafferSQ-Bold.woff2',
+      path: '../public/fonts/HafferSQ-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
